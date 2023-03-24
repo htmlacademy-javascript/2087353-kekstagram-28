e2d84e8438ac7be6878956b58b8cfbf0a0a080a8
Keks <keks@htmlacademy.ru>
2023-03-24 14:51:24 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/util.js
@@ -0,0 +1,41 @@
+import {onPopupClose, picturePopup, closePopupButton} from './popup.js';
+
+const getRandomInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+function createRandomIdFromRangeGenerator (min, max) {
+  const previousValues = [];
+
+  return function () {
+    let currentValue = getRandomInteger(min, max);
+    if (previousValues.length >= (max - min + 1)) {
+      console.error(`Перебраны все числа из диапазона от ${ min } до ${ max}`);
+      return null;
+    }
+    while (previousValues.includes(currentValue)) {
+      currentValue = getRandomInteger(min, max);
+    }
+    previousValues.push(currentValue);
+    return currentValue;
+  };
+}
+
+const onEscapeClose = (evt) => {
+  if (evt.key === 'Escape') {
+    evt.preventDefault();
+    picturePopup.classList.add('hidden');
+    document.body.classList.remove('modal-open');
+  }
+  document.removeEventListener('keydown', onEscapeClose);
+  closePopupButton.removeEventListener('click', onPopupClose);
+};
+
+export {
+  createRandomIdFromRangeGenerator,
+  getRandomInteger,
+  onEscapeClose
+};
